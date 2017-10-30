0286e5828fdaf8fb751919ba3e3bb530908f71f4
Fabian Starke <fabian@starke.fr>
2017-10-30 10:10:57 +0100

    fix(main js): minor fix

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ let runnerAge = 18;
 
 console.log(raceNumber)
 
-if(runnerRegisteredEarly){
-  console.log(raceNumber += 1000)
-} else if(runnerAge && runnerRegisteredEarly){
+if(!runnerRegisteredEarly){
+  console.log(raceNumber + 1000)
+
+if(runnerAge >= 18 && runnerRegisteredEarly){
   console.log(raceNumber + ':  You will run at 9:30')
-} else if(runnerRegisteredEarly || runnerAge){
+} else if(runnerRegisteredEarly || runnerAge >= 18){
   console.log(raceNumber + ' : You will run at 11:00 am ')
-} else if(runnerRegisteredEarly === false && runnerAge < 18){
+} else if(!runnerRegisteredEarly && runnerAge < 18){
   console.log(raceNumber + ' : You will run at 12:30 pm')
 } else {console.log('See registration desk')};
